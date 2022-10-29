1e5173248389f16daf5a29ca5a891283045504c4
NCTP <cjfgh530@naver.com>
2022-10-29 23:34:08 +0900

    Implementing Starwitch Bossfight
    Bomb Rain
    Lazer Beams
    Damage indicator

---
+++ ProjectW/Source/ProjectW/SWitch.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Enemy.h"
+//#include "PaperZD.h"
+#include "SWitch.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTW_API ASWitch : public APaperCharacter
+{
+	GENERATED_BODY()
+	
+};
